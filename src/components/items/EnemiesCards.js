2e93790e1c2eb652eb5b00afb2b70296e67798a2
Emilio Navarro <sainfuw@aol.com>
2022-03-18 13:09:39 -0300

    more items on row modal3

--- src/components/items/EnemiesCards.js
+++ src/components/items/EnemiesCards.js
@@ -79,7 +79,7 @@ const styles = {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 800,
+    width: "1050px",
     bgcolor: "background.paper",
     border: "2px solid #000",
     boxShadow: 24,
